58c65f6b11b0db12630f8fdde5212c5dd7708985
Marcus Ames <marcusames00@gmail.com>
2019-01-20 17:07:14 -0700

    finished checkpoint

--- src/main/java/com/galvanize/Algorithm.java
+++ src/main/java/com/galvanize/Algorithm.java
@@ -1,4 +1,40 @@
 package com.galvanize;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Algorithm {
+    public boolean allEqual(String input) {
+        boolean isEqual = true;
+        if (input.length() <= 0) {
+            isEqual = false;
+        }
+        ArrayList<String> letters = new ArrayList<>(Arrays.asList(input.toLowerCase().split("")));
+        String firstLetter = letters.get(0);
+        for (String letter : letters) {
+            if(!firstLetter.equals(letter)) {
+                isEqual = false;
+            }
+        }
+        return isEqual;
+    }
+
+    public HashMap<String, Integer> letterCount(String input) {
+        HashMap<String, Integer> lettersC = new HashMap<>();
+        if(input.length() <= 0) {
+            return lettersC;
+        }
+        ArrayList<String> letters = new ArrayList<>(Arrays.asList(input.toLowerCase().split("")));
+        letters.forEach(letter -> lettersC.put(letter, lettersC.getOrDefault(letter, 0) + 1));
+        return lettersC;
+    }
+
+    public String interleave(String[] a1, String[] a2) {
+        String acc = "";
+        for (int i = 0; i < a1.length; i++) {
+            acc += a1[i] + a2[i];
+        }
+        return acc;
+    }
 }
--- src/test/java/com/galvanize/AlgorithmTest.java
+++ src/test/java/com/galvanize/AlgorithmTest.java
@@ -1,4 +1,41 @@
 package com.galvanize;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static junit.framework.TestCase.*;
+
 public class AlgorithmTest {
+
+    @Test
+    public void allEqualTest() {
+        Algorithm algorithm = new Algorithm();
+        assertTrue(algorithm.allEqual("aAa"));
+        assertFalse(algorithm.allEqual("bbBbabbb"));
+        assertFalse(algorithm.allEqual(""));
+    }
+
+    @Test
+    public void letterCountTest() {
+        Algorithm algorithm = new Algorithm();
+        HashMap<String, Integer> test = new HashMap<>();
+        test.put("a", 2);
+        assertEquals(test, algorithm.letterCount("aA"));
+        HashMap<String, Integer> test2 = new HashMap<>();
+        test2.put("a", 1);
+        test2.put("b", 2);
+        test2.put("c", 1);
+        test2.put("d", 1);
+        assertEquals(test2, algorithm.letterCount("abBcd"));
+        assertEquals(new HashMap<>(),algorithm.letterCount(""));
+    }
+
+    @Test
+    public void interleaveTest() {
+        Algorithm algorithm = new Algorithm();
+        String[] a1 = new String[]{"h", "l", "o", "w", "r", "d"};
+        String[] a2 = new String[]{"e", "l", " ", "o", "l", "!"};
+        assertEquals("hello world!", algorithm.interleave(a1, a2));
+    }
 }
